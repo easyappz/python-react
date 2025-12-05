304ad6ad53a4967ee94e04c7bb1d786905e306f3
IbragimKhaniev <newinterny@gmail.com>
2025-12-05 03:11:50 +0300

    feat: update Home component with centered Hello World text

--- react/src/components/Home/index.jsx
+++ react/src/components/Home/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
-/**
- * Для ИИ: Это заглушка. Обнови этот компонент. Здесь должен быть начальный экран приложения.
- */
 export const Home = () => {
   return (
     <div data-easytag="id1-src/components/Home/index.jsx">
-      <div>This is home page</div>
+      <div style={{ 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        fontSize: '2.5rem',
+        fontWeight: 'bold',
+        color: '#333',
+        textAlign: 'center'
+      }}>
+        Привет, Мир!
+      </div>
     </div>
   );
 };
